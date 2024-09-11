ff5f52a496e7e15f5c4969c4d210f5e36de5c833
AnnikaRings <annika.rings@optellum.com>
2024-09-11 21:50:10 +0100

    added custom debug implementation

--- src/ast.rs
+++ src/ast.rs
@@ -1,15 +1,33 @@
+use std::fmt;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive( Clone, PartialEq)]
 pub enum SKI {
     S,
     K,
     I,
     Application(Box<App>),
 }
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct App {
     pub combinator: SKI,
     pub arg: SKI,
+}
+/// custom formatter for SKI
+impl fmt::Debug for SKI {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SKI::I => write!(f, "I"),
+            SKI::K => write!(f, "K"),
+            SKI::S => write!(f, "S"),
+            SKI::Application(x) => write!(f, "{:?}", x),
+        }
+    }
+}
+/// custom formatter for App
+impl fmt::Debug for App {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}{:?}", self.combinator, self.arg)
+    }
 }
 impl SKI {
     pub fn is_application(&self) -> bool {
